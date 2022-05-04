9619f5196e702e632dd66562b2688a145de4cafc
ConradMar3 <conradmare890317@gmail.com>
2022-05-04 20:44:32 +0200

    2022/05/04 (pm)

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,10 +5,10 @@ public class Hello {
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
-        int myTotal = myFirstNumber + mySecondNumber +  myThirdNumber;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.println(myTotal);
 
-        int myLastOne = 1000 - myTotal;
+        int myLastOne = -1000 + myTotal;
         System.out.println(myLastOne);
     }
 }
