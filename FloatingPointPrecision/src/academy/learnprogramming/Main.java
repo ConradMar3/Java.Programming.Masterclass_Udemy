0a188597018c5f37aeeaae424cbf1bdd54d29467
ConradMar3 <conradmare890317@gmail.com>
2022-05-07 18:23:30 +0200

    2022/05/07 (pm)
    FloatingPointPrecision.
    
    Signed-off-by: ConradMar3 <conradmare890317@gmail.com>

---
+++ FloatingPointPrecision/src/academy/learnprogramming/Main.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double numberOfPounds = 200d;
+        double convertdeKilograms = numberOfPounds * 0.45359237d;
+        System.out.println(convertdeKilograms);
+
+    }
+}
