d0f16d31eeb7481540daf85df358ac9acc37310e
ConradMar3 <conradmare890317@gmail.com>
2022-08-09 14:19:07 +0200

    2022/08/09 (pm)
    Section4.44 - Keywords and Expressions.

---
+++ KeywordsAndExpressions/src/com/timbuchalka/KeywordsAndExpressions1.java
@@ -0,0 +1,14 @@
+package com.timbuchalka;
+
+public class KeywordsAndExpressions1 {
+    public static void main(String[] args) {
+        int int2 = 5;
+        double kilometres = (100 * 1.609);
+
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
---
+++ KeywordsAndExpressions/src/com/timbuchalka/KeywordsAndExpressionsChallenge.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class KeywordsAndExpressionsChallenge {
+    public static void main(String[] args) {
+        int score = 100;
+
+        if(score > 99) {
+            System.out.println("You got the high score?");
+            score = 0;
+        }
+    }
+}
